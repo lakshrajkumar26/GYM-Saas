7b2098c49131a84433ad651da6373c0cb11baf46
Laksh Raj Kumar <lakshrajkumar@gmail.com>
2026-02-01 18:17:48 +0530

    with swagger

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const routes = require("./routes");
+const { swaggerUi, swaggerDocument } = require("./swagger");
 
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use("/api", routes);
